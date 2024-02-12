3b3a48b872c7efdb143ffa886c0bec809e511694
ben1998-hub johnson <ben.johnson1998@hotmail.co.uk>
2024-02-12 12:11:08 +0000

    Add climate questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,26 @@
 //Add questions here//
 var questions = [
-    {question:}
+    { question: "What is the primary cause of climate change?", Options: ["Solar Variability",
+     "Nartural Cycles", "Human Activity", "Orbital Variations"], correct: 2 },
+     {question: "Which gas is most significant greenhous gas?",Options:["Methane","Nitrous Oxide",
+      "Carbon Dioxide", "Chlorofluorocarbons"],correct:2},
+      {question:"Wath is the main source of rising sea levels?", options:["Glacier Melting", "Thermal Expansion of water",
+     "Increased Precipitation", "Deforestation"],correct:1},
+      {question:"Which sector is the largest emitter og green house gases?",Options:["Transportation","Agriculture", "Energy",
+     "Industrail prcesses"],correct:2},
+      {question:"What can individuals do to reduce their carbon footprint?", Options:["Use more plastic", "Increase meat consumption",
+     "Use public transport", "leave lights on"],correct:2},
+      {question:"Which renewable energy source is most widely used?", Options:["Solar power","Wind power", "Hydropower", 
+    "Geothermal energy"],correct:2},
+      {question:"What is the goal of the Paris Agreement?", Options:["Limit gobal rarming to 2 degrees Celsius", "promote coal use",
+     "Increase global temperatures", "Reduce biodiversity"],correct:0},
+      {question:"What phenomenon causes more intense and frequent weather events?", Options:["EL Niño", "Global Warming",
+     "La Niño", "Solar Flares" ],correct:1},
+      {question:"What is the impact of deforestation on the climate?",Options:["Increases oxygen", "Reduces Biodiversity",
+     "Lowers carbon dixide", "Contributes to global warming"],correct:3},
+      {question:"Which practice helps reduce carbon emissions in agriculture?",Options:["Monoculture farming",
+     "Overgrazing", "Sustainable farming", "Use of chemical fertilizers"],correct:2},
 ]
-document.getElementById('simple-button').addEventListener('click',function(){
+document.getElementById('simple-button').addEventListener('click', function () {
     alert('Button clicked!');
 })
